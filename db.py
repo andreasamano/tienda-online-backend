d2baf884e61680455280b89b49cc441c6f086438
Andrea <andreasamano@gmail.com>
2026-05-28 22:13:25 -0700

    Corregir variable SSL en db

--- db.py
+++ db.py
@@ -5,18 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    try:
-        conexion = mysql.connector.connect(
-            host=os.getenv('DB_HOST', '127.0.0.1'),
-            port=int(os.getenv('DB_PORT', 3306)),
-            user=os.getenv('DB_USER', 'movauser'),
-            password=os.getenv('DB_PASSWORD', '12345'),
-            database=os.getenv('DB_NAME', 'tienda_online'),
-                 ssl_disabled=not usar_ssl
-    )
-        return conexion
+    usar_ssl = os.getenv('DB_SSL', 'false').lower() == 'true'
 
-    except mysql.connector.Error as error:
-        print("ERROR AL CONECTAR A LA BASE DE DATOS:")
-        print(error)
-        raise error
+    return mysql.connector.connect(
+        host=os.getenv('DB_HOST', '127.0.0.1'),
+        port=int(os.getenv('DB_PORT', 3306)),
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD', ''),
+        database=os.getenv('DB_NAME', 'tienda_online'),
+        ssl_disabled=not usar_ssl
+    )
